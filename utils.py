64e08fa22d1146af24c71a9e5564e34e546f10da
Meybili <109104859+meybili19@users.noreply.github.com>
2025-07-11 18:29:24 -0500

    feat:

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 def preparar_datos_para_prediccion(df, meses_predecir=1):
-    df['fecha'] = pd.to_datetime(df['fecha'], format="%Y/%m")
+    df['fecha'] = pd.to_datetime(df['fecha'], errors='coerce')
+    df = df.dropna(subset=['fecha']) 
     df = df.sort_values('fecha')
     df = df[['fecha', 'valor']].copy()
     df['target'] = df['valor'].shift(-meses_predecir)
